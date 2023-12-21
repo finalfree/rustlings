0ce1b5043d88dd92dba0f69b62d436f1bcdf06c0
Carl Wang <Carl.Wang@wisetechglobal.com>
2023-12-22 01:08:27 +0800

    first day

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -13,9 +13,9 @@
 // Execute `rustlings hint intro1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
+    println!("this is my first rust hihi");
     println!("Hello and");
     println!(r#"       welcome to...                      "#);
     println!(r#"                 _   _ _                  "#);
